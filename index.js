1d691ef4ffaf02fa7e7217193201c12c97cefeb8
loquaz <sereno.desenvolvimento@gmail.com>
2021-11-24 20:00:07 -0300

    refatoracao

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import router from "./config/routes.js"
 import express from "express"
-import InvalidDigitableLineException from "./domain/exception/InvalidDigitableLineException.js"
-import ErrorJsonConverter from "./app/converter/ErrorJsonConverter.js"
 import ErrorConverterFactory from "./app/converter/ErrorConverterFactory.js"
 
 const server = express()
 
 server.use("/", router)
+
 server.use( (err, req, res, next) => {
+    
     const errorConverter = ErrorConverterFactory.getErrorConverter(err)
     res.status(400).send( errorConverter.convert( err ) )
     next()
-} )
+    
+})
 
 server.listen(3000, () => {
-    console.log("teste");
+    console.log("-----------------------------------------------");
+    console.log("--             Aplicacao iniciada            --");
+    console.log("-----------------------------------------------");
 })
